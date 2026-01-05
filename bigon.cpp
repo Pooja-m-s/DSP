cf48b4acaf610883afc0a48b1c8fa5db8a62f349
Pooja-m-s <shingarep839@gmail.com>
2026-01-06 00:13:47 +0530

    Change output to print 'Hello' in main function
    Updated the main function to print 'Hello' instead of array elements.

--- bigon.cpp
+++ bigon.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n = 5;
-    int arr[] = {1, 2, 3, 4, 5};
-
-    for(int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+int main(){
+    int n=5;
+    for(int i=0;i<n;i++) {
+        cout<<"Hello";
     }
-
     return 0;
 }
+
